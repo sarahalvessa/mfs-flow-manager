7f85aa06ba0be4f988b293f422e13db990e07338
sarahalvessa <sarah_030301@outlook.com>
2024-05-23 16:22:07 -0300

    feature: criação de cards e modal

---
+++ src/components/pessoa/dashboard/modals/ModalDashboard.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="modal" role="dialog" v-if="modalAberto">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">Cadastrar Nova Tarefa</h5>
+          <button type="button" class="close" @click="fecharModal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <div class="form-group">
+            <label for="titulo">Título da Tarefa</label>
+            <input type="text" class="form-control" id="titulo" v-model="titulo">
+          </div>
+          <div class="form-group">
+            <label for="descricao">Descrição</label>
+            <textarea class="form-control" id="descricao" v-model="descricao"></textarea>
+          </div>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-primary" @click="saveChanges">Salvar</button>
+          <button type="button" class="btn btn-secondary" @click="fecharModal">Fechar</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+ 
+<script>
+export default {
+  props: {
+    modalAberto: Boolean,
+  },
+  data() {
+    return {
+      titulo: '',
+      descricao: '',
+    }
+  },
+  methods: {
+    fecharModal() {
+      this.$emit('fechar')
+    },
+    saveChanges() {
+      this.$emit('salvar', {
+        titulo: this.titulo,
+        descricao: this.descricao,
+      })
+    },
+  },
+}
+</script>
+<style scoped>
+.modal {
+  display: block;
+}
+</style>
