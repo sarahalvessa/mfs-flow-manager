bd63d7c3e5889cd7f579c10b66363728dd2749a9
sarahalvessa <sarah_030301@outlook.com>
2024-10-16 16:58:36 -0300

    style: melhorias de estilo na tela de cadastro

--- src/pages/Auth/Register.vue
+++ src/pages/Auth/Register.vue
@@ -1,93 +1,116 @@
 <template>
-    <div class="register">
-      <h1>Cadastro</h1>
-      <form @submit.prevent="cadastrarUsuario">
-        <div>
-          <label for="nome">Nome</label>
-          <input type="text" id="nome" v-model="nome" required />
-        </div>
-        <div>
-          <label for="email">Email</label>
-          <input type="email" id="email" v-model="email" required />
-        </div>
-        <div>
-          <label for="password">Senha:</label>
-          <input type="password" id="password" v-model="password" required />
-        </div>
-        <button type="submit">Cadastrar</button>
-      </form>
+  <div class="div-1">
+    <div class="div-2">
+      <img :src="pessoa" alt="imagem de pessoa">
     </div>
-  </template>
-  
+    <div class="div-3">
+      <div class="div-4">
+        <img :src="logoFlow" alt="imagem de pessoa">
+      
+        <div class="div-5">
+          <form @submit.prevent="registerUser" class="div-6">
+            <h4 class="div-margin-top">Create an account</h4>
+            <p class="div-margin">Setup a new account in a minute.</p>
+            <Input v-model="name" type="text" placeholder="Name" class="div-margin"/>
+            <Input v-model="email" type="email" placeholder="Email" class="div-margin"/>
+            <Input v-model="password" type="password" placeholder="Password" class="div-margin"/>
+            <Button>Sign Up</Button>
+            <p class="div-margin div-margin-top">Already have an account?<router-link to="/login">Sign In</router-link></p>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
 <script>
-  import { post } from '/src/api.js'
-  
-  export default {
-    data() {
-      return {
-        nome: '',
-        email: '',
-        password: ''
+import { post } from '/src/api.js'
+import { useToast } from 'vue-toastification'
+import Input from '/src/components/Auth/Input.vue'
+import Button from '/src/components/Auth/Button.vue'
+
+export default {
+  components: {
+    Input,
+    Button
+  },
+  data() {
+    return {
+      name: '',
+      email: '',
+      password: '',
+      pessoa: require('/src/assets/pessoa.png'),
+      logoFlow: require('/src/assets/logoFlow.png')
+    }
+  },
+  methods: {
+    async registerUser() {
+      const toast = useToast()
+      if(this.name === '' || this.email === '' || this.password === '') {
+        return toast.error('Invalid Credentials')
+      }
+      try {
+        const payload = {
+          nome: this.name,
+          email: this.email,
+          password: this.password,
+        }
+        await post(`/api/register/`, payload)
+        toast.success('Success')
+        this.$router.push({ name: 'login'})
+      } catch (error) {
+        return toast.error('Invalid Credentials')
       }
     },
-    methods: {
-      async cadastrarUsuario() {
-        try {
-          const payload = {
-            nome: this.nome,
-            email: this.email,
-            password: this.password,
-          }
-          await post(`/api/register/`, payload)
-          this.$router.push({ name: 'login'})
-        } catch (error) {
-          console.error('Erro ao efetuar cadastro:', error)
-        }
-      },
-    }
   }
-  </script>
-  
-  <style scoped>
-  .register {
-    max-width: 400px;
-    margin: 0 auto;
-    padding: 1rem;
-    border: 1px solid #ccc;
-    border-radius: 5px;
-  }
-  
-  h1 {
-    text-align: center;
-  }
-  
-  form div {
-    margin-bottom: 1rem;
-  }
-  
-  label {
-    display: block;
-    margin-bottom: 0.5rem;
-  }
-  
-  input {
-    width: 100%;
-    padding: 0.5rem;
-    box-sizing: border-box;
-  }
-  
-  button {
-    width: 100%;
-    padding: 0.5rem;
-    background-color: #007bff;
-    color: white;
-    border: none;
-    border-radius: 5px;
-    cursor: pointer;
-  }
-  
-  button:hover {
-    background-color: #0056b3;
-  }
-  </style>
-  
+}
+</script>
+<style scoped>
+.div-margin-top {
+  margin-top: 30px;
+}
+.div-margin {
+  margin-bottom: 30px;
+}
+.div-1 {
+  height: 100vh;
+  display: flex;
+  flex-direction: row;
+}
+.div-2 {
+  width: 40%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.div-2 img {
+  max-width: 100%;
+  height: auto;
+}
+.div-3 {
+  width: 60%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.div-4 {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 50%;
+}
+.div-4 img {
+  max-width: 30%;
+}
+.div-5 {
+  border-radius: 20px;
+  border: 2px solid #cccccca8;
+  box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);
+}
+.div-6 {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+</style>
